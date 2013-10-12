faabc984cc3763f3d35218081cfa2871d89cb937
KetanPandya <ketan.pandya@rordynamics.com>
2013-10-12 15:32:44 +0100

    Added validation to ensure name are specified when creating projects

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 
 feature 'Creating Projects' do 
-	scenario "can create a project" do 
+
+	before do
 		visit '/'
 
-		click_link 'New Project' 
+		click_link 'New Project'
+	end
+
+	scenario "can create a project" do 
+
 
 		fill_in 'Name', with: 'TextMate 2'
 		fill_in 'Description', with: 'A text-edit for OS X'
@@ -14,12 +19,20 @@ feature 'Creating Projects' do
 	
 
 
-project = Project.where(name: "TextMate 2").first
+	project = Project.where(name: "TextMate 2").first
 
-expect(page.current_url).to eql(project_url(project))
+	expect(page.current_url).to eql(project_url(project))
 
-title = "TextMate 2"
+	title = "TextMate 2"
 
-expect(page).to have_title(title)
+	expect(page).to have_title(title)
+    end
+
+    scenario "cannot create a project without a name" do
+
+    	click_button 'Create Project'
+
+    	expect(page).to have_content("Project has not been created.")
+    	expect(page).to have_content("Name can't be blank")
 	end
 end
