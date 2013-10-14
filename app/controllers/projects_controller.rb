e586369be2005bd267b1ba42a5dc44574223e486
KetanPandya <ketan.pandya@rordynamics.com>
2013-10-14 22:37:11 +0100

    Redirect the users back to the projects page if they try going to a project  that doesn't exit.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,13 @@
 class ProjectsController < ApplicationController
 
+    before_action :set_project, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@projects = Project.all 
 	end
 
 	def show
-		@project = Project.find(params[:id])
+		
 	end
 
 
@@ -26,11 +28,10 @@ class ProjectsController < ApplicationController
 	end
 
 	def edit
-		@project = Project.find(params[:id])
+		
 	end
 
 	def update
-		@project = Project.find(params[:id])
 		
 		if @project.update(project_params)
 			flash[:notice] = "Project has been updated."
@@ -42,7 +43,6 @@ class ProjectsController < ApplicationController
 	end
 
 	def destroy
-		@project = Project.find(params[:id])
 
 		@project.destroy
 
@@ -52,6 +52,14 @@ class ProjectsController < ApplicationController
 	end
 
 	private
+
+	  def set_project
+	     @project = Project.find(params[:id])
+	  rescue ActiveRecord::RecordNotFound
+	  	flash[:alert] = "The project you were looking" +
+	  		" for could not be found."
+	  	redirect_to projects_path	
+	  end
 
 	  def project_params
 	  	params.require(:project).permit(:name, :description)
