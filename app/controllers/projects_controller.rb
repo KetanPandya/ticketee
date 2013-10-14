46c06ba14b36de3aec48412e4694e10293ecebbc
KetanPandya <ketan.pandya@rordynamics.com>
2013-10-14 21:32:45 +0100

    You can now update a project.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,11 @@ class ProjectsController < ApplicationController
 		@projects = Project.all 
 	end
 
+	def show
+		@project = Project.find(params[:id])
+	end
+
+
 	def new
 		@project = Project.new
 	end
@@ -20,8 +25,20 @@ class ProjectsController < ApplicationController
 		end
 	end
 
-	def show
+	def edit
 		@project = Project.find(params[:id])
+	end
+
+	def update
+		@project = Project.find(params[:id])
+		
+		if @project.update(project_params)
+			flash[:notice] = "Project has been updated."
+			redirect_to @project
+		else
+			flash[:alert] = "Project has not been updated."
+			render "edit"
+		end
 	end
 
 	private
